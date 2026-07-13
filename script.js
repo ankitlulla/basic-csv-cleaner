08335d812f35e386e32b95badb5bece688ccf62a
ankitlulla <ankit.lulla777@gmail.com>
2026-07-13 18:24:53 +0400

    Display selected file information

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("CSV Cleaner loaded!");
+const fileInput = document.getElementById("csvFile");
+const summary = document.getElementById("summary");
+
+fileInput.addEventListener("change", () => {
+
+    const file = fileInput.files[0];
+
+    if (!file) return;
+
+    summary.innerHTML = `
+        <p><strong>Selected File</strong></p>
+        <p>${file.name}</p>
+    `;
+
+});
