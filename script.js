80fc9f56f3e856082b9b4101267ed05fa28ad021
ankitlulla <ankit.lulla777@gmail.com>
2026-07-13 18:33:22 +0400

    Display uploaded CSV data

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const fileInput = document.getElementById("csvFile");
 const summary = document.getElementById("summary");
+const tableContainer = document.getElementById("tableContainer");
 
 fileInput.addEventListener("change", () => {
 
@@ -8,8 +9,52 @@ fileInput.addEventListener("change", () => {
     if (!file) return;
 
     summary.innerHTML = `
-        <p><strong>Selected File</strong></p>
-        <p>${file.name}</p>
+        <p><strong>Selected File:</strong> ${file.name}</p>
     `;
 
+    Papa.parse(file, {
+        header: true,
+        skipEmptyLines: true,
+
+        complete: function(results) {
+            displayTable(results.data);
+        }
+
+    });
+
 });
+
+function displayTable(data){
+
+    if(data.length === 0){
+        tableContainer.innerHTML = "<p>No data found.</p>";
+        return;
+    }
+
+    let table = "<table>";
+
+    table += "<tr>";
+
+    Object.keys(data[0]).forEach(column=>{
+        table += `<th>${column}</th>`;
+    });
+
+    table += "</tr>";
+
+    data.forEach(row=>{
+
+        table += "<tr>";
+
+        Object.values(row).forEach(value=>{
+            table += `<td>${value}</td>`;
+        });
+
+        table += "</tr>";
+
+    });
+
+    table += "</table>";
+
+    tableContainer.innerHTML = table;
+
+}
